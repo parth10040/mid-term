aa447d22ab43ffa600dde8584cd47de228dd31ac
Andy <chowzer@gmail.com>
2021-09-19 22:11:54 -0400

    App*.tsx: Adding Instructions in App.tsx on how to introduce an accessibility violation.  Adding jest-axe test in App.test.tsx that will detect the violation.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import App from './App';
+
+expect.extend(toHaveNoViolations);
 
 describe('App', () => {
 
@@ -8,5 +11,11 @@ describe('App', () => {
     render(<App />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
+  });
+
+  it('does not have jest-axe violations', async () => {
+    const { container } = render(<App />);
+
+    expect(await axe(container)).toHaveNoViolations();
   });
 });
