cdfb45005831ecbbf34392a298c6526c85fa9f41
Andy <chowzer@gmail.com>
2021-10-05 20:52:25 -0400

    server.ts: adding jokes API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import JokeApi from '../JokeApi'
 
 (async () => {
   const app = express();
+  const jokeBank = new JokeApi();
 
   app.use('/', (_, res) => {
     res.json('hello world');
+  });
+
+  app.use('getjokes', (_, res) => {
+    return res.json(jokeBank.getJokes());
   });
 
   app.listen(8000, () => {
