db70488cbaeda9123f5f1142ab3fe231af9a6e12
Andy <chowzer@gmail.com>
2021-10-06 21:35:10 -0400

    modal.tsx: updating modal

--- ui/src/modal/modal.tsx
+++ ui/src/modal/modal.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
 import "../index.css";
 
 export default function Modal(props: ModalProps) {
-  const { children } = props;
+  const { children, showBackButton } = props;
 
   return (
     <div className="modal">
       <div className="modal__container">
         <div className="modal__featured">
-          <button
-            type="button"
-            className="button--transparent button--close"
-          >
-            <svg className="nc-icon glyph" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="32px" height="32px" viewBox="0 0 32 32">
-              <g>
-                <path fill="#ffffff" d="M1.293,15.293L11,5.586L12.414,7l-8,8H31v2H4.414l8,8L11,26.414l-9.707-9.707 C0.902,16.316,0.902,15.684,1.293,15.293z"></path>
-              </g>
-            </svg>
-          </button>
+          {
+            showBackButton ?
+              <button
+                type="button"
+                className="button--transparent button--close"
+              >
+                <svg className="nc-icon glyph" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="32px" height="32px" viewBox="0 0 32 32">
+                  <g>
+                    <path fill="#ffffff" d="M1.293,15.293L11,5.586L12.414,7l-8,8H31v2H4.414l8,8L11,26.414l-9.707-9.707 C0.902,16.316,0.902,15.684,1.293,15.293z"></path>
+                  </g>
+                </svg>
+              </button> : <></>
+          }
           <img
             className="modal__product"
             src="https://source.unsplash.com/random/270x588"
@@ -34,4 +36,5 @@ export default function Modal(props: ModalProps) {
 
 interface ModalProps {
   children?: JSX.Element;
+  showBackButton: Boolean;
 }
