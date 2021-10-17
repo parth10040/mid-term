d511d223b058125108e825e420ec841f40347dd1
Andy <chowzer@gmail.com>
2021-10-17 15:09:57 -0400

    server.ts: implementing joke routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,35 @@
 import cors from 'cors';
-import express from 'express';
+import express, { json } from 'express';
 import JokeApi from '../JokeApi'
 
 (async () => {
   const app = express();
   app.use(cors());
+  app.use(json());
 
   const jokeBank = new JokeApi();
-  app.get('jokes', (_, res) => {
+  app.get('/jokes', (_, res) => {
     return res.json(jokeBank.getJokes());
+  });
+
+  app.post('/jokes', (req, res) => {
+    const { body } = req;
+
+    jokeBank.postJoke(body.content);
+    return res.json('success');
+  });
+
+  app.put('/jokes', (req, res) => {
+    const { body: { uuid, content } } = req;
+
+    jokeBank.updateJoke(uuid, content);
+    return res.json('success');
+  });
+
+  app.delete('/jokes', (req, res) => {
+    jokeBank.deleteJoke(req.body.uuid);
+
+    return res.json('success');
   });
 
   app.use('/', (_, res) => {
